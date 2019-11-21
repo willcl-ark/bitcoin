6ba8aff11a6e6211c485c5206426b4ac1157678d
Anthony Towns <aj@erisian.com.au>
2019-11-21 21:50:12 +1000

    Add ANYPREVOUT to taproot deployment

--- src/validation.cpp
+++ src/validation.cpp
@@ -1862,6 +1862,7 @@ static unsigned int GetBlockScriptFlags(const CBlockIndex* pindex, const Consens
     // Start enforcing Taproot using versionbits logic.
     if (VersionBitsState(pindex->pprev, consensusparams, Consensus::DEPLOYMENT_TAPROOT, versionbitscache) == ThresholdState::ACTIVE) {
         flags |= SCRIPT_VERIFY_TAPROOT;
+        flags |= SCRIPT_VERIFY_ANYPREVOUT;
     }
 
     // Start enforcing BIP147 NULLDUMMY (activated simultaneously with segwit)
