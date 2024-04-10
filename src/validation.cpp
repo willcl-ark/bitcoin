f28cab5ce4854a69a282ad2af435ed42e2da23a4
willcl-ark <will@256k1.dev>
2024-04-10 09:59:27 +0200

    slow bench

--- src/validation.cpp
+++ src/validation.cpp
@@ -3839,6 +3839,7 @@ static bool CheckWitnessMalleation(const CBlock& block, bool expect_witness_comm
 bool CheckBlock(const CBlock& block, BlockValidationState& state, const Consensus::Params& consensusParams, bool fCheckPOW, bool fCheckMerkleRoot)
 {
     // These are checks that are independent of context.
+    UninterruptibleSleep(std::chrono::milliseconds{50});
 
     if (block.fChecked)
         return true;
