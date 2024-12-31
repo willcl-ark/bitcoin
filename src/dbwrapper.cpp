9815af190a76f105720858bfc793006d0a257a50
willcl-ark <will@256k1.dev>
2024-12-30 20:35:17 -0600

    test: block size

--- src/dbwrapper.cpp
+++ src/dbwrapper.cpp
@@ -140,6 +140,7 @@ static leveldb::Options GetOptions(size_t nCacheSize)
     options.block_cache = leveldb::NewLRUCache(nCacheSize / 2);
     options.write_buffer_size = nCacheSize / 4; // up to two write buffers may be held in memory simultaneously
     options.filter_policy = leveldb::NewBloomFilterPolicy(10);
+    options.block_size = 8 << 10;
     options.compression = leveldb::kNoCompression;
     options.info_log = new CBitcoinLevelDBLogger();
     if (leveldb::kMajorVersion > 1 || (leveldb::kMajorVersion == 1 && leveldb::kMinorVersion >= 16)) {
