769f49d53551383f8c86c32f053978e245802214
will <will@256k1.dev>
2026-01-15 09:47:39 +0000

    lint: use uv
    https://docs.astral.sh/uv/

--- ci/lint/container-entrypoint.sh
+++ ci/lint/container-entrypoint.sh
@@ -10,7 +10,7 @@ export LC_ALL=C
 # of the mounted bitcoin src dir.
 git config --global --add safe.directory /bitcoin
 
-export PATH="/python_build/bin:${PATH}"
+export PATH="/python_env/bin:${PATH}"
 
 if [ -z "$1" ]; then
   bash -ic "./ci/lint/06_script.sh"
