b83d3de389033bea31f980e7953ae72179bdbe98
will <will@256k1.dev>
2026-01-16 09:50:31 +0000

    ci: install pyzmq in all CI environments

--- ci/test/00_setup_env_i686_no_ipc.sh
+++ ci/test/00_setup_env_i686_no_ipc.sh
@@ -10,6 +10,6 @@ export HOST=i686-pc-linux-gnu
 export CONTAINER_NAME=ci-i686-no-ipc
 export CI_IMAGE_NAME_TAG="mirror.gcr.io/debian:trixie"
 export CI_IMAGE_PLATFORM="linux/amd64"
-export PACKAGES="llvm clang g++-multilib"
+export PACKAGES="llvm clang g++-multilib python3-zmq"
 export DEP_OPTS="DEBUG=1 NO_IPC=1"
 export CI_LIMIT_STACK_SIZE=1
--- ci/test/00_setup_env_native_previous_releases.sh
+++ ci/test/00_setup_env_native_previous_releases.sh
@@ -9,7 +9,8 @@ export LC_ALL=C.UTF-8
 export CONTAINER_NAME=ci-native-previous-releases
 export CI_IMAGE_NAME_TAG="mirror.gcr.io/ubuntu:22.04"
 # Use minimum supported python3.10 and gcc-12, see doc/dependencies.md
-export PACKAGES="gcc-12 g++-12 python3-zmq wget"
+export PACKAGES="gcc-12 g++-12 python3-zmq python3-pip wget"
+export PIP_PACKAGES="pycapnp"
 export CI_CMAKE_INSTALL="true"
 export CMAKE_VERSION="3.24.3"
 export CMAKE_URL="https://github.com/Kitware/CMake/releases/download/v${CMAKE_VERSION}/cmake-${CMAKE_VERSION}-linux-x86_64.tar.gz"
