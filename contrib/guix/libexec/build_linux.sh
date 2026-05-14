0beba92beb6c1ec15765c955f52012e55d0214b0
fanquake <fanquake@gmail.com>
2026-05-14 12:11:17 +0100

    guix: build x86_64-linux bitcoind & utils statically
    -static-pie
    Produce a static position independent executable on targets that support it.
    A static position independent executable is similar to a static executable,
    but can be loaded at any address without a dynamic linker.
    
    See https://gcc.gnu.org/onlinedocs/gcc/Link-Options.html

--- contrib/guix/libexec/build_linux.sh
+++ contrib/guix/libexec/build_linux.sh
@@ -50,7 +50,7 @@ done
 
 glibc_dynamic_linker=$(
     case "$HOST" in
-        x86_64-linux-gnu)      echo /lib64/ld-linux-x86-64.so.2 ;;
+        x86_64-linux-gnu)      ;;
         arm-linux-gnueabihf)   echo /lib/ld-linux-armhf.so.3 ;;
         aarch64-linux-gnu)     echo /lib/ld-linux-aarch64.so.1 ;;
         riscv64-linux-gnu)     echo /lib/ld-linux-riscv64-lp64d.so.1 ;;
@@ -118,6 +118,7 @@ HOST_LDFLAGS="-Wl,--as-needed -Wl,--dynamic-linker=$glibc_dynamic_linker -Wl,-O2
 
 # EXE FLAGS
 case "$HOST" in
+    x86_64-linux-gnu) CMAKE_EXE_LINKER_FLAGS="-DCMAKE_EXE_LINKER_FLAGS=-static-pie -static-libgcc -Wl,-O2" ;;
     *linux*)  CMAKE_EXE_LINKER_FLAGS="-DCMAKE_EXE_LINKER_FLAGS=${HOST_LDFLAGS} -static-libstdc++ -static-libgcc" ;;
 esac
 
