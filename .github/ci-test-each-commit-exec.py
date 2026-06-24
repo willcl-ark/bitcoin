318674df16250fc5983f982b4b986d73785954c8
will <will@256k1.dev>
2026-06-24 17:07:32 +0100

    ci: use test-each-commit CMake preset
    ...in test-each-commit-exec.py

--- .github/ci-test-each-commit-exec.py
+++ .github/ci-test-each-commit-exec.py
@@ -24,25 +24,7 @@ def main():
     num_procs = int(run(["nproc"], stdout=subprocess.PIPE).stdout)
     build_dir = "ci_build"
 
-    run([
-        "cmake",
-        "-B",
-        build_dir,
-        "-Werror=dev",
-        # Use clang++, because it is a bit faster and uses less memory than g++
-        "-DCMAKE_C_COMPILER=clang",
-        "-DCMAKE_CXX_COMPILER=clang++",
-        # Use mold, because it is faster than the default linker
-        "-DCMAKE_EXE_LINKER_FLAGS=-fuse-ld=mold",
-        # Use Debug build type for more debug checks, but enable optimizations
-        "-DAPPEND_CXXFLAGS='-O3 -g2'",
-        "-DAPPEND_CFLAGS='-O3 -g2'",
-        "-DCMAKE_BUILD_TYPE=Debug",
-        "-DCMAKE_COMPILE_WARNING_AS_ERROR=ON",
-        "--preset=dev-mode",
-        # Tolerate unused (member) functions in intermediate commits in a pull request
-        "-DCMAKE_CXX_FLAGS=-Wno-error=unused-member-function -Wno-error=unused-function",
-    ])
+    run(["cmake", "--preset=ci-test-each-commit"])
 
     if run(["cmake", "--build", build_dir, "-j", str(num_procs)], check=False).returncode != 0:
         print("Build failure. Verbose build follows.")
