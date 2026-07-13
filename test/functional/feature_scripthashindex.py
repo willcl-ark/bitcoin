4edfdccd79dc7d97f10c53357f5bdecec9b6e80f
will <will@256k1.dev>
2026-07-13 12:25:11 +0100

    test: cover shared scripthash spend mode
    Add unit coverage comparing standalone scripthash spend markers with the
    new txospender-backed resolver.
    
    Extend the functional scripthash test with a node running both indexes and
    compare its RPC results against the scripthash-only node for funding and
    spending flows.

--- test/functional/feature_scripthashindex.py
+++ test/functional/feature_scripthashindex.py
@@ -18,16 +18,26 @@ def scripthash(script_pub_key):
 class ScriptHashIndexTest(BitcoinTestFramework):
     def set_test_params(self):
         self.setup_clean_chain = True
-        self.num_nodes = 2
-        self.extra_args = [[], ["-scripthashindex"]]
+        self.num_nodes = 3
+        self.extra_args = [
+            [],
+            ["-scripthashindex"],
+            ["-scripthashindex", "-txospenderindex"],
+        ]
 
-    def wait_for_index(self, height):
-        self.wait_until(lambda: self.nodes[1].getindexinfo()["scripthashindex"]["synced"])
-        self.wait_until(lambda: self.nodes[1].getindexinfo()["scripthashindex"]["best_block_height"] >= height)
+    def wait_for_index(self, node, height):
+        self.wait_until(lambda: node.getindexinfo()["scripthashindex"]["synced"])
+        self.wait_until(lambda: node.getindexinfo()["scripthashindex"]["best_block_height"] >= height)
+
+    def wait_for_shared_indexes(self, node, height):
+        self.wait_for_index(node, height)
+        self.wait_until(lambda: node.getindexinfo()["txospenderindex"]["synced"])
+        self.wait_until(lambda: node.getindexinfo()["txospenderindex"]["best_block_height"] >= height)
 
     def run_test(self):
         no_index_node = self.nodes[0]
         node = self.nodes[1]
+        shared_node = self.nodes[2]
         wallet = MiniWallet(node)
 
         zero_scripthash = "00" * 32
@@ -41,9 +51,11 @@ class ScriptHashIndexTest(BitcoinTestFramework):
         )
         self.start_node(0)
         self.connect_nodes(0, 1)
+        self.connect_nodes(1, 2)
 
         self.generate(wallet, 101)
-        self.wait_for_index(node.getblockcount())
+        self.wait_for_index(node, node.getblockcount())
+        self.wait_for_shared_indexes(shared_node, node.getblockcount())
 
         assert_equal(node.getscripthashactivity(zero_scripthash), {
             "history": [],
@@ -52,13 +64,21 @@ class ScriptHashIndexTest(BitcoinTestFramework):
             "bestblock": node.getbestblockhash(),
             "height": node.getblockcount(),
         })
+        assert_equal(shared_node.getscripthashactivity(zero_scripthash), {
+            "history": [],
+            "utxos": [],
+            "balance": 0,
+            "bestblock": shared_node.getbestblockhash(),
+            "height": shared_node.getblockcount(),
+        })
 
         _, script_pub_key, _ = getnewdestination()
         amount = 123456789
         tx = wallet.send_to(from_node=node, scriptPubKey=script_pub_key, amount=amount)
         block_hash = self.generate(node, 1)[0]
         block_height = node.getblockheader(block_hash)["height"]
-        self.wait_for_index(block_height)
+        self.wait_for_index(node, block_height)
+        self.wait_for_shared_indexes(shared_node, block_height)
 
         activity = node.getscripthashactivity(scripthash(script_pub_key))
         assert_equal(activity["height"], block_height)
@@ -71,13 +91,16 @@ class ScriptHashIndexTest(BitcoinTestFramework):
             "height": block_height,
             "value": amount,
         }])
+        assert_equal(shared_node.getscripthashactivity(scripthash(script_pub_key)), activity)
+
         wallet_script_pub_key = wallet.get_output_script()
         wallet_scripthash = scripthash(wallet_script_pub_key)
         receive_tx = wallet.send_to(from_node=node, scriptPubKey=wallet_script_pub_key, amount=amount)
         receive_block = self.generate(node, 1)[0]
         receive_height = node.getblockheader(receive_block)["height"]
         wallet.rescan_utxos()
-        self.wait_for_index(receive_height)
+        self.wait_for_index(node, receive_height)
+        self.wait_for_shared_indexes(shared_node, receive_height)
 
         received_activity = node.getscripthashactivity(wallet_scripthash)
         assert_equal(received_activity["history"][-1], {"txid": receive_tx["txid"], "height": receive_height})
@@ -88,6 +111,7 @@ class ScriptHashIndexTest(BitcoinTestFramework):
             "value": amount,
         }
         assert received_utxo in received_activity["utxos"]
+        assert_equal(shared_node.getscripthashactivity(wallet_scripthash), received_activity)
 
         spend_tx = wallet.send_self_transfer(
             from_node=node,
@@ -95,11 +119,13 @@ class ScriptHashIndexTest(BitcoinTestFramework):
         )
         spend_block = self.generate(node, 1)[0]
         spend_height = node.getblockheader(spend_block)["height"]
-        self.wait_for_index(spend_height)
+        self.wait_for_index(node, spend_height)
+        self.wait_for_shared_indexes(shared_node, spend_height)
 
         spent_activity = node.getscripthashactivity(wallet_scripthash)
         assert {"txid": spend_tx["txid"], "height": spend_height} in spent_activity["history"]
         assert received_utxo not in spent_activity["utxos"]
+        assert_equal(shared_node.getscripthashactivity(wallet_scripthash), spent_activity)
 
         node.invalidateblock(spend_block)
         assert_raises_rpc_error(-1, "scripthash index is catching up", node.getscripthashactivity, wallet_scripthash)
