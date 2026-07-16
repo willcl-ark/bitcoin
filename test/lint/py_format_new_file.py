5fc93522a1d69d52cec8d57a61fdeb2ba969eb42
will <will@256k1.dev>
2026-07-16 15:36:32 +0100

    test: require formatting for new Python files
    Add a valid Python fixture with a formatting-only violation so new files
    
    cannot bypass the py_format check.

---
+++ test/lint/py_format_new_file.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# Copyright (c) The Bitcoin Core developers
+# Distributed under the MIT software license, see the accompanying
+# file COPYING or https://opensource.org/license/mit/.
+"""Fixture for the incremental Python formatting lint check."""
+
+FORMAT_FIXTURE = 'new file'
